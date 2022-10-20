06c75b9e03d27a033ebce139ade883062a3c0d56
Yuyi Tang <yuyitang@yuyis-mbp.mynetworksettings.com>
2022-10-19 23:31:38 -0400

    Finish 4 GETS 2 POSTS

--- src/application.py
+++ src/application.py
@@ -9,10 +9,22 @@ app = Flask(__name__)
 
 CORS(app)
 
+@app.route("/api/carts", methods=["GET"])
+def get_all_carts():
 
-@app.route("/api/carts/<id>", methods=["GET"])
+    size = request.args.get('size', 20)
+    result = ShoppingResource.get_carts(int(size))
+
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+
+@app.route("/api/carts_id/<id>", methods=["GET"])
 def get_carts_by_id(id):
-
 
     result = ShoppingResource.get_by_key(id)
 
@@ -23,11 +35,24 @@ def get_carts_by_id(id):
 
     return rsp
 
-@app.route("/api/items/<id>", methods=["GET"])
+@app.route("/api/items", methods=["GET"])
+def get_items():
+
+    size = request.args.get('size', 20)
+    result = ShoppingResource.get_items(int(size))
+
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+@app.route("/api/items_id/<id>", methods=["GET"])
 def get_items_by_id(id):
 
     result = ShoppingResource.get_by_item_id(id)
-    
+
     if result:
         rsp = Response(json.dumps(result), status=200, content_type="application.json")
     else:
@@ -36,8 +61,49 @@ def get_items_by_id(id):
     return rsp
 
 
+@app.route("/api/items_name/<name>", methods=["GET"])
+def get_items_by_name(name):
+
+    size = request.args.get('size', 20)
+    result = ShoppingResource.get_by_item_name(name, size)
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
 
 
+@app.route("/api/items/create", methods=["POST"])
+def create_item():
+
+    request_json = request.get_json()
+    item_id = request_json.get('item_id')
+    name = request_json.get('item_name')
+    description = request_json.get('description')
+    result = ShoppingResource.create_item(item_id, description, name)
+
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("ITEM NOT CREATED", status=404, content_type="text/plain")
+
+    return rsp
+
+
+@app.route("/api/carts/create", methods=["POST"])
+def create_cart():
+    request_json = request.get_json()
+    user_id = request_json.get('user_id')
+    cart_id = request_json.get('cart_id')
+    result = ShoppingResource.create_cart(user_id, cart_id)
+
+    if result:
+        rsp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        rsp = Response("CART NOT CREATED", status=404, content_type="text/plain")
+
+    return rsp
 
 
 
--- src/shopping_resource.py
+++ src/shopping_resource.py
@@ -1,3 +1,4 @@
+import flask
 import pymysql
 
 import os
@@ -32,6 +33,32 @@ class ShoppingResource:
         return result
 
     @staticmethod
+    def get_carts(size):
+        sql = "SELECT * FROM Cart.carts LIMIT %s"
+        conn = ShoppingResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=size)
+        result = cur.fetchall()
+
+        l1 = []
+        for r in result:
+            l1.append(r)
+
+        return l1
+
+    @staticmethod
+    def get_items(size):
+
+        sql = "SELECT * FROM Cart.items LIMIT %s"
+        conn = ShoppingResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=size)
+        result = cur.fetchall()
+
+        return result
+
+
+    @staticmethod
     def get_by_item_id(key):
 
         sql = "SELECT * FROM Cart.items WHERE Cart.items.item_id=%s"
@@ -41,3 +68,42 @@ class ShoppingResource:
         result = cur.fetchone()
 
         return result
+
+    @staticmethod
+    def get_by_item_name(name, size):
+
+        sql = "SELECT * FROM Cart.items WHERE Cart.items.item_name=%s LIMIT %s"
+        conn = ShoppingResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, (name, size))
+        result = cur.fetchall()
+
+        l1 = []
+        for r in result:
+            l1.append(r)
+
+        return l1
+
+
+    @staticmethod
+    def create_item(item_id, description, name):
+
+        sql = "INSERT INTO Cart.items(item_id, item_name, description) VALUES(%s, %s, %s)"
+        conn = ShoppingResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, (item_id, name, description))
+        conn.commit()
+
+        return {'item_id': item_id, 'item_name': name, 'description': description}
+
+    @staticmethod
+    def create_cart(user_id, cart_id):
+        sql = "INSERT INTO Cart.carts(user_id, cart_id) VALUES(%s, %s)"
+        conn = ShoppingResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, (user_id, cart_id))
+        conn.commit()
+
+        return {'user_id': user_id, 'cart_id': cart_id}
+
+
